0d3a90cacd3fe95c88983d5725a50d6a5a02ca4f
wanglinwd <wanglinwd@yeah.net>
2021-12-26 20:55:35 +0800

    add a out

--- gitmodou/src/com/git/test/CommitJavaGit.java
+++ gitmodou/src/com/git/test/CommitJavaGit.java
@@ -2,6 +2,6 @@ package com.git.test;
 
 public class CommitJavaGit {
         public static void  main (String[] args) {
-                System.out.println("1234");
+                System.out.println("123422222");
         }
 }
